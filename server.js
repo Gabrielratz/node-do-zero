b84ce47356373b237b7bbd2b373b620f6ec9d625
Gabrielratz <gabrielpavan08@hotmail.com>
2025-08-27 21:12:08 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,61 @@
+// import { createServer } from 'node:http'
+
+//const server = createServer((req, res)=>{
+ //   res.write ('oi')
+   // return res.end()
+//})
+
+//server.listen(3333)
+
+
+import {fastify} from 'fastify'
+//import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database.postgres.js'
+
+const server = fastify ()
+//const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+
+ server.post('/videos', async(request, reply)=>{
+   const {title, description, duration} = request.body
+ await database.create({
+    title: title,
+    description: description,
+    duration: duration
+})
+
+
+return reply.status(201).send()
+
+})
+
+server.get('/videos',async (request)=>{
+
+const search =request.query.search
+const videos = await database.list(search)
+
+return videos
+})
+
+server.put('/videos/:id', async(request, reply)=>{
+const videoID = request.params.id
+const {title, description, duration} = request.body
+await database.update(videoID, {
+    title,
+    description,
+    duration
+})
+return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, reply)=>{
+const videoID = request.params.id
+await database.delete(videoID)
+
+return reply.status(204).send
+})
+
+server.listen({ 
+    port: process.env.PORT ?? 3333
+})
